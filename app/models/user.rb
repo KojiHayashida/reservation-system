2bf8288228675f3335f2832f4f5bec918ffda057
Kikita <kikita1211main@gmail.com>
2023-02-20 13:00:14 +0900

    管理画面で新規予約の入力フォームひな形を作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
   has_many :reservation
   extend DisplayList
+
+  ## validations
+  # validates :first_name,:family_name, presence: true,
+  #                                     format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は漢字/ひらがな/カタカナで入力してください"},
+  #                                     length: { maximum: 10, allow_blank: true }
+  # validates :first_name_reading,:family_name_reading, presence: true,
+  #                                                     format: { with: /\A[ぁ-んー－]+\z/, message: "はひらがなで入力してください" },
+  #                                                     length: { maximum: 10, allow_blank: true }
+  # validates :password, presence: true,
+  #                      length: { minimum: 8 },
+  #                      format: {with: /\A[\w\-]+\z/, message: "は半角英数字8文字以上で入力してください"},
+  #                      allow_nil: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, # Question　registable これ無効にしても大丈夫？
