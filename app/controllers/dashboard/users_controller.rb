523ccd769a15a8f7fae5cf6c98b32a3c39c56adc
Kikita <kikita1211main@gmail.com>
2023-04-19 20:42:17 +0900

    新規利用者登録ページを分けた

--- app/controllers/dashboard/users_controller.rb
+++ app/controllers/dashboard/users_controller.rb
@@ -20,17 +20,17 @@ class Dashboard::UsersController < ApplicationController
         @reservations = Reservation.where("user_id = ?", params[:id]).order(day: :asc)
     end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            redirect_to dashboard_users_path, notice: "ユーザー作成に成功しました."
-        else
-            render :new
-        end
-    end
-
     def edit
 
+    end
+
+    def create
+        user = User.new
+        user.assign_attributes(user_params)
+        user.password = 'password'
+        user.password_confirmation = 'password'
+        user.save
+        redirect_to dashboard_users_path, notice: "ユーザー作成に成功しました."
     end
 
     def update
