413ec19dbdd3d56748235b1b6753d239f594d3d4
Kikita <kikita1211main@gmail.com>
2023-05-16 22:04:44 +0900

    modify datestamp

--- db/migrate/20230218064222_create_reservations.rb
+++ db/migrate/20230218064222_create_reservations.rb
@@ -4,8 +4,8 @@ class CreateReservations < ActiveRecord::Migration[5.2]
       t.date "day", null: false
       t.string "time", null: false
       t.bigint "user_id", null: false
-      t.datetime "created_at",  null: false
-      t.datetime "updated_at",  null: false
+      # t.datetime "created_at",  null: false
+      # t.datetime "updated_at",  null: false
       t.datetime "start_time"
       t.index ["user_id"], name: "index_reservations_on_user_id"
       t.timestamps
