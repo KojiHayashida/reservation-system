627fb427bc31017d003820cf392942fc82434a1d
Kikita <kikita1211main@gmail.com>
2023-05-18 18:45:02 +0900

    modify congfig

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
 
   ## https://qiita.com/uenoryo/items/5dd69436e9f5af6a752e 即時反映させるやつ
